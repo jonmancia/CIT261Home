ea3fb1c830bb5b9c0c3655da579833988faec0db
jonmancia <jonathanman2010@icloud.com>
2020-02-22 00:36:27 -0700

    Bug fixed. Completed

--- js/animations.js
+++ js/animations.js
@@ -1,52 +1,41 @@
-const state = new State()
 // mousemove listener
 document.body.addEventListener('mousemove', mouseHandler)
 // get ball element
 const ballEl = document.querySelector('.ball')
 function mouseHandler(e) {
+    // get the position of the ball (x and y coordinates)
     const ballPos = ballEl.getBoundingClientRect()
 
+    // get the position of the cursor
     const cursorPos = {
         name: 'cursor',
         x: e.clientX,
         y: e.clientY,
     }
-    const ball = {
-        name: 'ball',
-        x: ballPos.x,
-        y: ballPos.y,
-    }
-
-    const distanceFromCenter = calcDistance(getCenter(ballPos), cursorPos)
+    // Move the ball
     moveBall(ballPos, cursorPos)
 }
 
 function moveBall(ballPos, cursorPos) {
-    const windowWidth = window.innerWidth
-    const windowHeight = window.innerHeight
-    // if the distance of x and y is equla or less than 120,
-    // then get the coordinates of the center of the circle and add 25px for both x and y
+    // get the height and width of container where ball is found
+    const windowWidth = document.querySelector('.ball-container').offsetWidth
+    const windowHeight = document.querySelector('.ball-container').offsetHeight
+
+    // if the total distance of x and y is equal or less than 120,
+    // then get the coordinates of the center of the circle and randomize the position in x and y
     if (calcDistance(getCenter(ballPos), cursorPos).total <= 120) {
+        // if the distance from the cursor's x and the ball's x is less than or equal to 150,
+        // change the position of the ball
         if (calcDistance(getCenter(ballPos), cursorPos).x <= 150) {
             const randomWidth = Randomize(windowWidth)
             const randomHeight = Randomize(windowHeight)
-            console.log('random: ', randomHeight, randomWidth)
-            console.log('height: ', windowHeight, windowWidth)
-
+            // assign random width and height
             ballEl.style.left = randomWidth + 'px'
             ballEl.style.top = randomHeight + 'px'
-            console.log(getCenter(ballPos))
-            // if (getCenter(ballPos).x >= windowWidth + 'px') {
-            //     ballEl.style.top(randomWidth - windowWidth)
-            //     console.log('p')
-            // } else if (getCenter(ballPos).y >= windowHeight + 'px') {
-            //     ballEl.style.left(randomHeight - windowHeight)
-            //     console.log('h')
-            // }
         }
     }
 }
-
+// get center of ball
 function getCenter(ballPos) {
     let centerX = ballPos.width / 2 + ballPos.x
     let centerY = ballPos.height / 2 + ballPos.y
@@ -54,6 +43,7 @@ function getCenter(ballPos) {
     return centerCoords
 }
 
+// calculate the combine distance between the cursor's coordinates and the ball's coordinates
 function calcDistance(centerCoords, cursorCoords) {
     let xDistance = Math.abs(centerCoords.x - cursorCoords.x)
     let yDistance = Math.abs(centerCoords.y - cursorCoords.y)
@@ -61,19 +51,7 @@ function calcDistance(centerCoords, cursorCoords) {
     return { total: totalDistance, x: xDistance, y: yDistance }
 }
 
+// randomizes up to parameter specified inclusive
 function Randomize(num) {
     return Math.floor(Math.random() * num) + 1
-}
-
-function State() {
-    this.x = 0
-    this.y = 0
-
-    this.setState = function(key, value) {
-        this[key] += value
-    }
-
-    this.getState = function(key) {
-        return this[key]
-    }
 }

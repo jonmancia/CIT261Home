d5d27aaee5014f828c9659bb31f18402bc3934ce
jonmancia <jonathanman2010@icloud.com>
2020-03-21 22:58:49 -0600

    final project minor countries complete

--- js/final.js
+++ js/final.js
@@ -1,8 +1,9 @@
 /* Object definitions Start */
 // Location class definition
 class Location {
-    constructor(country = null, state = null) {
+    constructor(country = null, country_code = 0, state = null) {
         this.country = country
+        this.country_code = country_code
         this.state = state
     }
 }
@@ -23,6 +24,7 @@ class DOMElements {
         this.deathsEl = document.querySelector('#deaths')
         this.recoveredEl = document.querySelector('#recovered')
         this.locationEl = document.querySelector('#location')
+        this.locationTextEl = document.querySelector('#location-text')
     }
 }
 
@@ -33,7 +35,7 @@ class Request {
     }
 
     getData() {
-        if (!this.location) {
+        if (this.location == null || this.location.country == -1) {
             return fetch(
                 `https://cors-anywhere.herokuapp.com/https://coronavirus-tracker-api.herokuapp.com/v2/latest`
             )
@@ -41,13 +43,37 @@ class Request {
                     return response.json()
                 })
                 .then(data => data)
-        } else {
-            fetch(
-                `https://coronavirus-tracker-api.herokuapp.com/v2/${this.location}`
-            )
-                .then(data => data.json())
-                .then(response => console.log(response))
+        } else if (this.location.country) {
+            return fetch(
+                `https://coronavirus-tracker-api.herokuapp.com/v2/locations?country_code=${this.location.country_code}`
+            ).then(data => {
+                return data.json()
+            })
         }
+    }
+
+    getCountryData() {
+        return fetch(
+            'https://coronavirus-tracker-api.herokuapp.com/v2/locations'
+        )
+            .then(res => res.json())
+            .then(data => {
+                // Map of all locations by country
+                const locationMap = {}
+                const locations = data.locations
+                // Loop through each obj and create a map
+                // of all locations include states/provinces
+                // the keys of map are country names
+                for (let i = 0; i < locations.length; i++) {
+                    if (!locationMap[locations[i]['country']]) {
+                        locationMap[locations[i]['country']] = []
+                        locationMap[locations[i]['country']].push(locations[i])
+                    } else {
+                        locationMap[locations[i]['country']].push(locations[i])
+                    }
+                }
+                return locationMap
+            })
     }
 }
 /* Object definitions End */
@@ -58,11 +84,38 @@ class Request {
 document.querySelector('body').onload = function() {
     // get elements
     const elements = new DOMElements()
+
     // fetch data
-    const req = new Request().getData()
-    req.then(data => renderOnDom(data, elements))
+    const req = new Request()
+
     // renderOnDom
+    req.getData().then(data => renderOnDom(data, elements))
+
+    // get Country, country id, and country_code
+    req.getCountryData().then(data => {
+        console.log(data)
+        bindToDropdown(data, elements.locationEl)
+    })
 }
+
+document.querySelector('#location').addEventListener('change', function(e) {
+    // get elements
+    const elements = new DOMElements()
+
+    // change location-text
+    elements.locationTextEl.textContent =
+        e.target.options[e.target.options.selectedIndex].textContent
+
+    // Create a location
+    const location = new Location(
+        e.target.options[e.target.options.selectedIndex].textContent,
+        e.target.options[e.target.options.selectedIndex].value
+    )
+    // pass location
+    const req = new Request(location)
+    // make request with location
+    req.getData().then(data => renderOnDom(data, elements))
+})
 
 /* Listeners End */
 
@@ -71,4 +124,23 @@ function renderOnDom({ latest }, elements) {
     elements.confirmedEl.textContent = latest.confirmed.toLocaleString()
     elements.deathsEl.textContent = latest.deaths.toLocaleString()
     elements.recoveredEl.textContent = latest.recovered.toLocaleString()
+}
+// Sort data from response by alphabetical order
+function sortDataByKey(data) {
+    const orderedData = {}
+    Object.keys(data)
+        .sort()
+        .forEach(key => (orderedData[key] = data[key]))
+    return orderedData
+}
+// Bind countries to dropdown
+function bindToDropdown(data, element) {
+    const sortedData = sortDataByKey(data)
+    for (let country in sortedData) {
+        const el = document.createElement('option')
+        el.setAttribute('value', sortedData[country][0]['country_code'])
+        const text = document.createTextNode(country)
+        el.appendChild(text)
+        element.appendChild(el)
+    }
 }

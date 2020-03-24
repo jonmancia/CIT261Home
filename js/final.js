5f327d895de5e0b319752120233aba15c99e10fc
jonmancia <jonathanman2010@icloud.com>
2020-03-24 12:27:12 -0600

    updated functions to work with recent third-party API changes - code complete

--- js/final.js
+++ js/final.js
@@ -51,6 +51,7 @@ class Request {
                     return response.json()
                 })
                 .then(data => data)
+                .catch(err => alert(err))
         } else if (this.location.id) {
             return fetch(
                 `https://cors-anywhere.herokuapp.com/https://coronavirus-tracker-api.herokuapp.com/v2/locations/${this.location.id}`
@@ -59,37 +60,72 @@ class Request {
                     return response.json()
                 })
                 .then(data => data)
+                .catch(err => alert(err))
         } else if (this.location.country) {
             return fetch(
                 `https://coronavirus-tracker-api.herokuapp.com/v2/locations?country_code=${this.location.country_code}`
-            ).then(data => {
-                return data.json()
-            })
+            )
+                .then(data => {
+                    return data.json()
+                })
+                .catch(err => alert(err))
         }
     }
 
     getCountryData() {
-        return fetch(
-            'https://coronavirus-tracker-api.herokuapp.com/v2/locations'
-        )
-            .then(res => res.json())
-            .then(data => {
-                // Map of all locations by country
-                const locationMap = {}
-                const locations = data.locations
-                // Loop through each obj and create a map
-                // of all locations include states/provinces
-                // the keys of map are country names
-                for (let i = 0; i < locations.length; i++) {
-                    if (!locationMap[locations[i]['country']]) {
-                        locationMap[locations[i]['country']] = []
-                        locationMap[locations[i]['country']].push(locations[i])
-                    } else {
-                        locationMap[locations[i]['country']].push(locations[i])
+        if (this.location && this.location.country == 'US') {
+            return fetch(
+                'https://coronavirus-tracker-api.herokuapp.com/v2/locations?source=csbs'
+            )
+                .then(res => res.json())
+                .then(data => {
+                    const stateMap = {}
+                    data['locations'].reduce((acc, current) => {
+                        if (!stateMap[current['state']]) {
+                            return (stateMap[current['state']] = {
+                                confirmed: current['latest']['confirmed'],
+                                deaths: current['latest']['deaths'],
+                                recovered: current['latest']['recovered'],
+                            })
+                        }
+                        stateMap[current['state']]['confirmed'] +=
+                            current['latest']['confirmed']
+                        stateMap[current['state']]['deaths'] +=
+                            current['latest']['deaths']
+                        stateMap[current['state']]['recovered'] +=
+                            current['latest']['recovered']
+                    }, stateMap)
+                    return stateMap
+                })
+                .catch(err => alert(err))
+        } else {
+            return fetch(
+                'https://coronavirus-tracker-api.herokuapp.com/v2/locations'
+            )
+                .then(res => res.json())
+                .then(data => {
+                    // Map of all locations by country
+                    const locationMap = {}
+                    const locations = data.locations
+                    // Loop through each obj and create a map
+                    // of all locations include states/provinces
+                    // the keys of map are country names
+                    for (let i = 0; i < locations.length; i++) {
+                        if (!locationMap[locations[i]['country']]) {
+                            locationMap[locations[i]['country']] = []
+                            locationMap[locations[i]['country']].push(
+                                locations[i]
+                            )
+                        } else {
+                            locationMap[locations[i]['country']].push(
+                                locations[i]
+                            )
+                        }
                     }
-                }
-                return locationMap
-            })
+                    return locationMap
+                })
+                .catch(err => alert(err))
+        }
     }
 }
 /* Object definitions End */
@@ -126,22 +162,33 @@ document
         elements.countryTextEl.textContent =
             e.target.options[e.target.options.selectedIndex].textContent
 
+        const countryName =
+            e.target.options[e.target.options.selectedIndex].textContent
+        const countryId = e.target.options[e.target.options.selectedIndex].value
+
         // Create a location
-        const location = new Location(
-            e.target.options[e.target.options.selectedIndex].textContent,
-            e.target.options[e.target.options.selectedIndex].value
-        )
+        const location = new Location(countryName, countryId)
 
         // pass location
         const req = new Request(location)
         // make request with location
-        req.getCountryData().then(data => {
-            bindStatesToDropdown(
-                data[location.country],
-                elements.stateLocationEl
-            )
-        })
-        req.getData().then(data => renderOnDom(data, elements))
+
+        if (countryName == 'US') {
+            req.getCountryData().then(data => {
+                const obj = {}
+                obj['latest'] = data
+                bindStatesToDropdown(obj, elements.stateLocationEl)
+            })
+            // render data on DOM
+        } else {
+            req.getCountryData().then(data => {
+                bindStatesToDropdown(
+                    data[location.country],
+                    elements.stateLocationEl
+                )
+            })
+            req.getData().then(data => renderOnDom(data, elements))
+        }
     })
 
 // State dropdown listener
@@ -165,10 +212,21 @@ document
         elements.stateTextEl.textContent =
             e.target.options[e.target.options.selectedIndex].textContent + ', '
 
-        // get data and render
-        req.getData().then(({ location }) => {
-            renderOnDom(location, elements)
-        })
+        if (
+            elements.countryLocationEl.options[
+                elements.countryLocationEl.options.selectedIndex
+            ].textContent != 'US'
+        ) {
+            // get data and render
+            req.getData().then(({ location }) => {
+                renderOnDom(location, elements)
+            })
+        } else {
+            const stateInfo = JSON.parse(localStorage.getItem('stateInfo'))
+            const obj = {}
+            obj['latest'] = stateInfo[id]
+            renderOnDom(obj, elements)
+        }
     })
 /* Listeners End */
 
@@ -200,30 +258,44 @@ function bindCountriesToDropdown(data, element) {
 
 // Bind states/provinces to dropdown
 function bindStatesToDropdown(data, element) {
-    // unbind previous states/provinces
+    // unbind previous states/provincess
     const elements = new DOMElements()
     for (let i = elements.stateLocationEl.options.length - 1; i >= 0; i--) {
         if (elements.stateLocationEl.options[i].value != '-1')
             elements.stateLocationEl.remove(i)
     }
-
-    // alphabetize list
-    const sortedData = data.sort((a, b) =>
-        a['province'] > b['province'] ? 1 : -1
-    )
-
-    // add states/provinces to dropdown
-    for (let state in sortedData) {
-        if (
-            // this regex necessary as not only states but cities are returned from api call
-            !sortedData[state]['province'].match(/,/) &&
-            sortedData[state]['province'].length != 0
-        ) {
+    let sortedData
+    if (data['latest']) {
+        sortedData = sortDataByKey(data.latest)
+        // save sorted data to local storage to later pull from it
+        localStorage.setItem('stateInfo', JSON.stringify(sortedData))
+        for (let state in sortedData) {
             const el = document.createElement('option')
-            el.setAttribute('value', sortedData[state]['id'])
-            const text = document.createTextNode(sortedData[state]['province'])
+            el.setAttribute('value', state)
+            const text = document.createTextNode(state)
             el.appendChild(text)
             element.appendChild(el)
+        }
+    } else {
+        // alphabetize list
+        sortedData = data.sort((a, b) =>
+            a['province'] > b['province'] ? 1 : -1
+        )
+        // add states/provinces to dropdown
+        for (let state in sortedData) {
+            if (
+                // this regex necessary as not only states but cities are returned from api call
+                !sortedData[state]['province'].match(/,/) &&
+                sortedData[state]['province'].length != 0
+            ) {
+                const el = document.createElement('option')
+                el.setAttribute('value', sortedData[state]['id'])
+                const text = document.createTextNode(
+                    sortedData[state]['province']
+                )
+                el.appendChild(text)
+                element.appendChild(el)
+            }
         }
     }
 }
